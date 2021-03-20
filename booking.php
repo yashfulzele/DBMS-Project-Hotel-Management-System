7c16ed722dba3e7d3482d31477c253733c4eca01
user_13 <28-user_13@users.noreply.ccgit.in>
2021-03-20 20:25:23 +0530

    booking commit6

--- booking.php
+++ booking.php
@@ -30,7 +30,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="reg-styles.css">
+    <link rel="stylesheet" href="styles.css">
     <title>Guest Booking</title>
 </head>
 <body style="background-color:rgb(0, 0, 0);">
